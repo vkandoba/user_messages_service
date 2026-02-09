91e3b5f753aa6d1589859bd8a8e148cdabac195b
vkandoba <viktor.kandoba@collectly.co>
2026-02-09 22:03:52 +0100

    added test and fix welcome

--- _tests/user_message_service_api_tests.py
+++ _tests/user_message_service_api_tests.py
@@ -5,14 +5,14 @@ from user_message_service_main import app, user_message_service
 client = TestClient(app)
 
 
-def test_ingest_event():
+def test_ingest_payment_failed_event():
     payment_failed_event = {
         "user_id": "u_12345",
         "event_type": "payment_failed",
         "event_timestamp": "2025-10-31T19:22:11Z",
         "properties": {
             "amount": 1425.00,
-            "attempt_number": 2,
+            "attempt_number": 4,
             "failure_reason": "INSUFFICIENT_FUNDS"
         },
         "user_traits": {
@@ -27,6 +27,7 @@ def test_ingest_event():
     assert response.status_code == 200
     messages = response.json()
     [m.pop("timestamp") for m in messages]
+    print(messages)
     assert messages == [
     {
         "message": {
@@ -38,5 +39,47 @@ def test_ingest_event():
         },
         'suppress_reason': None,
         "status": "sent",
+    },
+    {
+        'message': {
+            'channel': 'internal_alert',
+            'name': 'payment_retry_exhausted_alert',
+            'reason': 'At least third failed payment attempt detected',
+            'template': 'HIGH_RISK_ALERT',
+            'user_id': 'u_12345'
+        },
+        'status': 'sent',
+        'suppress_reason': None
     }
 ]
+
+
+def test_signup_completed():
+    payment_failed_event = {
+        "user_id": "u_12345",
+        "event_type": "signup_completed",
+        "event_timestamp": "2025-10-31T19:22:11Z",
+        "properties": {
+        },
+        "user_traits": {
+            "marketing_opt_in": True,
+        }
+    }
+
+    response = client.post("/api/v1/event", json=payment_failed_event)
+    assert response.status_code == 200
+    messages = response.json()
+    [m.pop("timestamp") for m in messages]
+    print(messages)
+    assert messages == [
+        {
+            'message': {
+                'channel': 'email',
+                'name': 'welcome',
+                'reason': 'New user signed up and opted in for marketing',
+                'template': 'WELCOME_EMAIL',
+                'user_id': 'u_12345'},
+            'status': 'sent',
+            'suppress_reason': None
+        }
+    ]
